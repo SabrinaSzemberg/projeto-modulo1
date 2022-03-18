78493cadf4aaa18c96846c9171c08c2c2c2079d7
SabrinaSzemberg <sabrina.schenberg@gmail.com>
2022-03-17 22:33:11 -0300

    adicionei media queries na página inicial e fiz ajustes no background

---
+++ tanjiro/js/fase2.js
@@ -0,0 +1,15 @@
+function tanjiroFaseI () {
+    while (opcao != 1 || opcao != 2) {
+        var opcao = prompt (`Se, durante uma corrida de carros, você deixa o segundo colocado pra trás, qual é a sua colocação após a ultrapassagem?\n 1- Segundo colocado\n 2- Último colocado`);
+
+        if (opcao == 1) {
+            window.location.href = "../html/fase3.html";
+            break;
+        } else if (opcao == 2) {
+            window.location.href = "../html/game-over.html";
+            break;
+        }else {
+            alert (`Escolha a opção 1 ou 2`);
+        }
+    }
+}
